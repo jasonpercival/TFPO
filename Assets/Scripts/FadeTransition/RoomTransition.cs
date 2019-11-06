5465954cb38ae9563acdce6f16b31150928eaec8
Christopher Brine <45048368+CreamyDuckling@users.noreply.github.com>
2019-11-06 16:21:41 -0500

    Made Smooth Fade Transition

---
+++ Assets/Scripts/FadeTransition/RoomTransition.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomTransition : MonoBehaviour {
+    public Animator animator;
+    private int levelIndex = 0;
+
+    public void FadeToLevel(int levelIndex) {
+        this.levelIndex = levelIndex;
+        animator.SetTrigger("fadeOut");
+    }
+
+    public void OnFadeComplete() {
+        SceneManager.LoadScene(levelIndex);
+    }
+}
