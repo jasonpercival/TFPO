41538fe9cbfac21e8ab9e95fd353b1cc38945f10
Christopher Brine <45048368+CreamyDuckling@users.noreply.github.com>
2019-11-06 15:01:50 -0500

    AudioManager Added

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+    // Singleton Variable
+    public static AudioManager instance;
+
+    public Sound[] sounds;
+
+    public void Awake() {
+        // Ensures only one instance of the AudioManager exists at a time
+        if (instance != null) {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(instance);
+
+        // Load in every sound
+        foreach (Sound s in sounds) {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.loop = s.loop;
+        }
+    }
+
+    // Use the start method for testing if necessary
+    //public void Start() {
+    //    PlaySound("EntryAmbience");
+    //}
+
+    // Attemps to find a sound the name to play it
+    public void PlaySound(string name) {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        // Failsafe in case a sound doesn't exist
+        if (s == null) {
+            Debug.Log("Sound doesn't exist!");
+            return;
+        }
+        s.source.Play();
+    }
+
+    // Attempts to find a sound given the name to stop if from playing
+    public void StopSound(string name) {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        // Failsafe in case a sound doesn't exist
+        if (s == null) {
+            Debug.Log("Sound doesn't exists");
+            return;
+        }
+        // Only stop the sound if it is actually playing
+        if (s.source.isPlaying) {
+            s.source.Stop();
+        }
+    }
+
+    // Stop all sounds that are currently playing
+    public void StopAllSounds() {
+        foreach(Sound s in sounds) {
+            StopSound(s.name);
+        }
+    }
+}
---
+++ Assets/Scripts/Audio/Sound.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Sound {
+    [HideInInspector]
+    public AudioSource source;
+    public AudioClip clip;      // The audio file itself
+    public string name;         // The name given to the sound in the inspector
+    public bool loop;           // If true, the sound will loop until it is stopped
+}
